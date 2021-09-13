68b4bb606bfb4985dc62dc5a494dde40b5eaa4db
liaojunjie <936118815@qq.com>
2021-09-13 14:37:26 +0800

    kScreenSize

--- YYKit/Base/Quartz/YYCGUtilities.m
+++ YYKit/Base/Quartz/YYCGUtilities.m
@@ -57,17 +57,21 @@ CGFloat YYScreenScale() {
 }
 
 CGSize YYScreenSize() {
-    static CGSize size;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        size = [UIScreen mainScreen].bounds.size;
-        if (size.height < size.width) {
-            CGFloat tmp = size.height;
-            size.height = size.width;
-            size.width = tmp;
-        }
-    });
-    return size;
+    return [UIScreen mainScreen].bounds.size;
+    /**
+     static CGSize size;
+     static dispatch_once_t onceToken;
+     dispatch_once(&onceToken, ^{
+         size = [UIScreen mainScreen].bounds.size;
+         if (size.height < size.width) {
+             CGFloat tmp = size.height;
+             size.height = size.width;
+             size.width = tmp;
+         }
+     });
+     return size;
+     */
+    
 }
 
 // return 0 when succeed
